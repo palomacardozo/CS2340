797eeeb465f53d661265349497390a2aff18f5b5
Shanaya Bharucha <sbharucha3@gatech.edu>
2024-09-18 17:06:04 -0400

    This is my first attempt to follow a Google Maps API tutorial. Currently it does not work

--- AtlantaFoodFinder/urls.py
+++ AtlantaFoodFinder/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-
 from . import views
+from .views import *
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    #path("", views.index, name="index"),
+    path("", HomeView.as_view(), name="my_home_view"),
 ]
--- AtlantaFoodFinder/views.py
+++ AtlantaFoodFinder/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView
+import googlemaps
+import json
+from django.conf import settings
+from .models import *
+
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the AtlantaFoodFinder index.")
+
+class HomeView(ListView):
+    template_name = 'home.html'
+    context_object_name = 'mydata'
+    model = Locations
+    #form_class = EmailForm
+    success_url = "/"
