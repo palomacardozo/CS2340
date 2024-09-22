a7c3b85a08e782ba6a381edb64d3e4b1e554cad5
erosenfeld7 <erosenfeld7@gatech.edu>
2024-09-21 22:30:14 -0400

    got admin site up and working and users being able to create accounts on sign up

--- users/views.py
+++ users/views.py
@@ -8,7 +8,9 @@ def signup(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('home')
+            referer = request.META.get('HTTP_REFERER',
+                                       'admin')  # Replace 'default_view_name' with a fallback URL
+            return redirect(referer + "AtlantaFoodFinder/")
     else:
         form = SignupForm()
     return render(request, 'users/signup.html', {'form': form})
