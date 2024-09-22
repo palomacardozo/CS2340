e244141837d71c8cdb778d51f7ec504f7bc8cbf8
erosenfeld7 <erosenfeld7@gatech.edu>
2024-09-22 11:13:45 -0400

    fixed sign up redirect issue so no more 404 error

--- users/views.py
+++ users/views.py
@@ -1,16 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from .forms import SignupForm
-
+from AtlantaFoodFinder.views import index
 def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
-            referer = request.META.get('HTTP_REFERER',
-                                       'admin')  # Replace 'default_view_name' with a fallback URL
-            return redirect(referer + "AtlantaFoodFinder/")
+            return redirect(index)
     else:
         form = SignupForm()
     return render(request, 'users/signup.html', {'form': form})
