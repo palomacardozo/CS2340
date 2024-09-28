dd7dc73985dce2829e6c27fcb4a1712f36673ed6
ayoun8 <150699484+ayoun8@users.noreply.github.com>
2024-09-28 05:23:02 -0400

    Creating a favorites model to link the favorites with the user

--- users/models.py
+++ users/models.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
+from AtlantaFoodFinder.models import Locations
 
 class CustomUser(AbstractUser):
     groups = models.ManyToManyField(
@@ -14,3 +16,11 @@ class CustomUser(AbstractUser):
         blank=True,
         verbose_name='user permissions',
     )
+
+class Favorite(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    restaurant = models.ForeignKey(Locations, on_delete=models.CASCADE)
+    added_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.restaurant.name}"
