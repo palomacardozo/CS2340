bf59a38604b351569ac2dd3baf38fd96d6bfaeba
ayoun8 <150699484+ayoun8@users.noreply.github.com>
2024-09-29 21:58:38 -0400

    Made some changes, also made map shorter

--- CS2340/settings.py
+++ CS2340/settings.py
@@ -126,12 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # Main static directory
-]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Where collectstatic puts files for production
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
