0ae6c1f4f3c3d3b15984452667e893877471b1f8
ayoun8 <150699484+ayoun8@users.noreply.github.com>
2024-09-29 21:12:24 -0400

    Finishing backend and frontend, made java script, put functionalities.

--- CS2340/settings.py
+++ CS2340/settings.py
@@ -125,7 +125,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # Main static directory
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Where collectstatic puts files for production
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
